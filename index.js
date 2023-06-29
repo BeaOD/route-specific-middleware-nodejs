dfa94a2251ab23ffaef78eeff6fc0f0400945341
BeaOD <owusubea@gmail.com>
2023-06-29 20:53:03 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+//general and route specific middlewares
+
+const express = require('express');
+const server = express()
+
+//request handling methods 
+const handleAllTypesOfRequests =(req,res) =>{
+    res.send('welcome to profile')
+}
+const handleProfilePutRequestType =(req,res) =>{
+    res.send('profile put request type')
+}
+const handleLoginGetRequest = (req, res) =>{
+    res.send('hello, this na login page')
+}
+const handleHomeGetRequest =(req,res) =>{
+    res.send('homepage dis')
+}
+
+// example of general middleware
+const middlewarefunction = (req, res, next) => {
+    console.log(req)
+    //console.log('this is the middleware');
+     //make some checks
+    // res.send ('this is middleware response')
+     
+    next();
+}
+
+//route specific middleware
+const loginRouteSpecificMiddleware =(req,res,next) =>{
+    console.log('login route specific middleware executed')
+    next();
+}
+
+//middleware
+//server.use(middlewarefunction);
+
+//routes
+server.post('/profile', handleAllTypesOfRequests )
+server.get('/login',loginRouteSpecificMiddleware, handleLoginGetRequest)
+server.put('/profile', handleProfilePutRequestType)
+server.post('/signup', handleHomeGetRequest)
+server.get('/',(req,res) => res.send('from signup for get method'))
+
+
+
+server.listen(3000, () => console.log('server is ready'))
